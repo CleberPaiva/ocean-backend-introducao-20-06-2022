a882a9755d1aa8d92ceccc2a65048e2404a3d692
Cleber Paiva <58832413+CleberPaiva@users.noreply.github.com>
2022-06-21 11:26:16 -0300

    Endpoint Read by ID - [GET] /herois/:id

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-console.log("******************");
-console.log("** Hello World! **");
-console.log("******************");
+const express = require("express");
+const app = express();
+
+//Informa que esta senso usado o json
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro", "Homem Aranha", "Super Homem"];
+
+// Endpoint Read All - [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(herois);
+});
+
+// Endpoint Read by ID - [GET] /herois/:id
+app.get("/herois/:id", function (req, res){
+  const id = req.params.id;
+
+  const item = herois[id - 1];
+
+  res.send(item);
+  console.log(item);
+});
+
+// Endpoint Create - [Post] /herois
+app.post("/herois", function (req, res){
+  console.log(req.body.nome);
+
+  // Acessa o nome do herói no corpo da requisição
+  const item = req.body.nome;
+
+  // Insere o item no array
+  herois.push(item);
+
+  console.log(herois);
+  res.send("Item inserido com sucesso!");
+});
+
+app.listen(3000, () =>
+  console.log("Servidor rodando em http://localhost:3000")
+);
+
+
